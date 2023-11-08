b36eb5d9f80de7106d9e65f00f83c87d8bf7e5bd
noeliawalterpro <noeliawalterpro@gmail.com>
2023-11-07 23:01:53 -0500

    feat: add actions calculator

--- app/src/main/java/com/noeliawalterpro/calculator/MainActivity.java
+++ app/src/main/java/com/noeliawalterpro/calculator/MainActivity.java
@@ -5,9 +5,13 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.view.View;
 import android.content.Intent;
+import android.widget.EditText;
+import android.widget.Spinner;
+import android.widget.Adapter;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -23,6 +27,9 @@ public class MainActivity extends AppCompatActivity {
         textInput2 = findViewById(R.id.textInput2);
         calculatorActions =findViewById(R.id.calculatorActions);
 
+        String[] Actions = {"ADD", "DIVISION", "MULTIPLY", "SUBTRACT"};
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(this, android.R.layout.simple_spinner_item, Actions);
+        calculatorActions.setAdapter(adapter);
 
         btnCalculate = findViewById(R.id.btn_calculate);
         btnCalculate.setOnClickListener(new View.OnClickListener() {
