808d95e739c9e750e839fe979f29c2ebd60012be
noeliawalterpro <noeliawalterpro@gmail.com>
2023-11-07 22:49:27 -0500

    feat: capture field inputs

--- app/src/main/java/com/noeliawalterpro/calculator/MainActivity.java
+++ app/src/main/java/com/noeliawalterpro/calculator/MainActivity.java
@@ -11,11 +11,18 @@ import android.content.Intent;
 
 public class MainActivity extends AppCompatActivity {
 
+    EditText textInput1,textInput2;
+    Spinner calculatorActions;
     Button btnCalculate;
 
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        textInput1 = findViewById(R.id.textInput1);
+        textInput2 = findViewById(R.id.textInput2);
+        calculatorActions =findViewById(R.id.calculatorActions);
+
 
         btnCalculate = findViewById(R.id.btn_calculate);
         btnCalculate.setOnClickListener(new View.OnClickListener() {
