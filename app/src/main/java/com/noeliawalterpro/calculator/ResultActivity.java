33156bc41995af938e94804f0178f2d75e1fbb5a
noeliawalterpro <noeliawalterpro@gmail.com>
2023-11-07 22:06:42 -0500

    feat: main activity and result code

--- app/src/main/java/com/noeliawalterpro/calculator/ResultActivity.java
+++ app/src/main/java/com/noeliawalterpro/calculator/ResultActivity.java
@@ -3,12 +3,26 @@ package com.noeliawalterpro.calculator;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.view.View;
+import android.content.Intent;
 
 public class ResultActivity extends AppCompatActivity {
 
-    @Override
+    Button btnReturn;
+
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_result);
+
+        btnReturn = findViewById(R.id.btn_return);
+        btnReturn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(ResultActivity.this, MainActivity.class);
+                startActivity(intent);
+            }
+
+        });
     }
 }
